f34348566486d70938d4ee15ef41e79e12155402
Suchith16 <97574776+Suchith16@users.noreply.github.com>
2022-11-26 20:10:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+prompt("what is your name..?");
+prompt("what is crush's name..?");
+var loveScore = Math.random() * 100 ;
+loveScore = Math.floor(loveScore);
+console.log(loveScore);
+alert ("your lovescore is " + loveScore +"%")
